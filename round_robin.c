c9aff9e738b58de6f96d8742f0ad08de46b6a433
tylzars <tyler@zars.me>
2026-04-09 20:25:14 -0400

    Actually add the round robin starting code...

---
+++ round_robin.c
@@ -0,0 +1,19 @@
+#include <stdint.h>
+#include "round_robin.h"
+
+uint8_t task_stacks[MAX_TASKS][MAX_STACK_SIZE];
+task_t tasks[MAX_TASKS];
+uint8_t num_tasks = 0;
+
+void create_task(void* task_entry) {
+    tasks[num_tasks].entry = task_entry;
+    tasks[num_tasks].sp = (uint32_t*)(&task_stacks[num_tasks][MAX_STACK_SIZE]);
+
+    tasks[num_tasks].ctx.SP = (uint32_t)(&task_stacks[num_tasks][MAX_STACK_SIZE]);
+    num_tasks++;
+    return;
+}
+
+__attribute__((naked)) void PendSV_Handler(void) {
+    return;
+}
